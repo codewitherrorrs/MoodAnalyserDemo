58bfc3dd41b4e8afa3855dc40c46a80eaf18c564
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-03 13:59:51 +0530

    Add : Reflection to Create MoodAnalyser with Parameter Construction

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Dynamic;
+using System.Reflection;
 
 namespace MoodAnalyserDemo
 {
@@ -7,9 +8,18 @@ namespace MoodAnalyserDemo
        
         public static void Main(string[] args)
         {
-        /*    Console.WriteLine("Enter the Mood : ");
+           Console.WriteLine("Enter the Mood : ");
             string mood = Console.ReadLine();
-            try
+            MoodAnalyser m1 = new MoodAnalyser(mood);
+
+            Type t = typeof(MoodAnalyser);
+            ConstructorInfo[] info = t.GetConstructors(BindingFlags.Public|BindingFlags.Instance);
+            foreach (ConstructorInfo info2 in info)
+            {
+                Console.WriteLine(info2.Name);
+            }
+               
+         /* try
             {
                 MoodAnalyser m1 = new MoodAnalyser();
                 m1.check(mood);
