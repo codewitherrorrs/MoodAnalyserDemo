41cefd043d8637535b184d8d573ee37e50a436fc
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-03 12:04:34 +0530

    Add : Reflection with Default Constructor

--- MoodAnalyser.cs
+++ MoodAnalyser.cs
@@ -8,30 +8,10 @@ namespace MoodAnalyserDemo
 {
     public class MoodAnalyser
     {
-        string mood; 
-       public void MoodException(string mood) 
+        string mood;
+        public MoodAnalyser() 
         {
-            if(mood == null) 
-            {
-                Console.WriteLine("Invalid Mood");
-                throw new Exception("Invalid Mood Found Exception");
-            }
-            else if(mood == "")
-            {
-                throw new Exception("Invalid Empty Mood Found Exception");
-            }
-            else if(mood == "Happy" || mood == "happy")
-            {
-                Console.WriteLine("Happy Mood");
-            }
-            else if(mood == "Sad" || mood == "sad")
-            {
-                Console.WriteLine("Sad Mood");
-            }
-            else 
-            {
-                Console.WriteLine("The mood is " + mood);
-            }
+            Console.WriteLine("Default Constructor");
         }
     }
 }
